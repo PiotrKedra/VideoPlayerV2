7cf5f4f797b773707017c88f71b6fc3987da330e
PiotrKedra <piotr.k9750@gmail.com>
2018-04-24 22:07:35 +0200

    lisiner not working

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,13 +1,18 @@
 package main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,9 +21,13 @@ public class Controller implements Initializable{
     @FXML private MediaView mediaView;
     private MediaPlayer mediaPlayer;
     private Media media;
+
+    @FXML private Button playPause;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String path =  new File("/home/pawel/Pobrane/sample.mp4").getAbsolutePath();
+        String path =  new File("C:/Users/piotr/Desktop/tgt.mp4").getAbsolutePath();
         media = new Media(new File(path).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
@@ -27,5 +36,16 @@ public class Controller implements Initializable{
         DoubleProperty height=mediaView.fitHeightProperty();
         width.bind(Bindings.selectDouble(mediaView.sceneProperty(),"width"));
         height.bind(Bindings.selectDouble(mediaView.sceneProperty(),"height"));
+
+        //lisiner nie dziala ;0
+        playPause.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
     }
+
+
+
 }
