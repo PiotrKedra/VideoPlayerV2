e71a98e0a448bada7644352909142830368cdee3
PiotrKedra <piotr.k9750@gmail.com>
2018-04-28 00:09:31 +0200

    thinking of gui look

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,15 +1,19 @@
 package main;
 
-import javafx.event.ActionEvent;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
@@ -23,7 +27,14 @@ public class Controller implements Initializable{
     private MediaPlayer mediaPlayer;
     private Media media;
 
+    @FXML private HBox bottomHBox;
+    @FXML private GridPane bottomMenu;
     @FXML private Button playPause;
+    @FXML private Button scrollBack;
+    @FXML private Button scrollForward;
+    @FXML private Button fastScrollBack;
+    @FXML private Button fastScrollForward;
+    @FXML private Button fullScreen;
 
 
     @Override
@@ -33,13 +44,12 @@ public class Controller implements Initializable{
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
         mediaPlayer.setAutoPlay(true);
-        DoubleProperty width=mediaView.fitWidthProperty();
+       /* DoubleProperty width=mediaView.fitWidthProperty();
         DoubleProperty height=mediaView.fitHeightProperty();
         width.bind(Bindings.selectDouble(mediaView.sceneProperty(),"width"));
-        height.bind(Bindings.selectDouble(mediaView.sceneProperty(),"height"));
+        height.bind(Bindings.selectDouble(mediaView.sceneProperty(),"height"));*/
 
 
-        //need to change for lambdas but how xd
         /*playPause.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -48,6 +58,15 @@ public class Controller implements Initializable{
         });*/
         //this is lambda ;o, shorter but...
         playPause.setOnAction((event) -> playOrPause());
+        scrollBack.setOnAction(event -> scroll(-5000));
+        scrollForward.setOnAction(event -> scroll(5000));
+        fastScrollBack.setOnAction(event -> scroll(-20000));
+        fastScrollForward.setOnAction(event -> scroll(20000));
+        fullScreen.setOnAction(event -> setFullScreen());
+
+        bottomHBox.setAlignment(Pos.CENTER);
+       // bottomBar.getStylesheets().add("style/BottomBar.css");
+       // playPause.getStylesheets().add("style/BottomBar.css");
 
     }
 
@@ -61,6 +80,24 @@ public class Controller implements Initializable{
         }
     }
 
+    private void scroll(int millis){
+        if(mediaPlayer!=null) {
+            Duration currentTime = mediaPlayer.getCurrentTime();
+            currentTime = currentTime.add(new Duration(millis));
+            mediaPlayer.seek(currentTime);
+        }
+    }
+
+    private void setFullScreen(){
+        Stage stage = Main.getStage();
+        stage.setFullScreen(!stage.isFullScreen());
+        if(Main.getStage().isFullScreen()){
+            //menu have to disapear
+        }else {
+            //menu gave to be back
+        }
+
+    }
 
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,15 +8,25 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         Scene scene = new Scene(root);
+ //       scene.getStylesheets().add("style/BottomBar.css");
+        primaryStage.setMinWidth(710);
+        primaryStage.setMinHeight(530);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+
+    public static Stage getStage() {
+        return stage;
+    }
 
     public static void main(String[] args) {
         launch(args);
